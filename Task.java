b95c6d9c9671fe91b5dc9396fbd1a89a76f84e55
einarfrog <einarzarate@ciencias.unam.mx>
2025-06-14 20:15:47 -0600

    Defining de Task class

---
+++ Task.java
@@ -0,0 +1,12 @@
+public class Task{
+  public String description;
+  public int duration;
+  public boolean done;
+  
+  public Task(String description,int duration){
+    this.description = description;
+    this.duration = duration;
+    this.done = false;
+  }
+
+}
